a84d313e240c9be5e980816f9b0258dae9d4c300
Akane Chan <getyourflag@gmail.com>
2020-04-24 21:03:49 +0800

    Added university logos

--- src/lib/logo.js
+++ src/lib/logo.js
@@ -3,13 +3,21 @@ import USTLogo from "../assets/UST.svg";
 import BULogo from "../assets/BU_logo.png";
 import LULogo from "../assets/LU_logo.svg";
 import CULogo from "../assets/CUHK_logo.png";
+import EDULogo from "../assets/eduhk.svg";
+import HKULogo from "../assets/HKU.svg";
+import OULogo from "../assets/ouhk.png";
+import PolyULogo from "../assets/PolyU.svg";
 
 const logos = {
     "CityU": cityULogo,
     "UST": USTLogo,
     "BU": BULogo,
     "LU": LULogo,
-    "CU": CULogo
+    "CU": CULogo,
+    "EDU": EDULogo,
+    "HKU": HKULogo,
+    "OU": OULogo,
+    "PolyU": PolyULogo
 }
 
 export default logos;
